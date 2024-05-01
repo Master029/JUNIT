3e5cced3790a09ee315b10dfbc4bf86650399c52
Master029 <ladotecnologico@gmail.com>
2024-05-01 12:56:38 +0200

    first commit

---
+++ src/angelSubtilJimenez/SistemaVentaEntradasTest.java
@@ -0,0 +1,17 @@
+package angelSubtilJimenez;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SistemaVentaEntradasTest {
+
+	@Test
+	@DisplayName("Hola"");
+	añadirSala(numero: int, peli: String): boolean {
+		assertEquals(0, null);
+	}
+
+}
+}
